47b1b8fdb920deaf34ac1977c83c414dea0f21ac
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-08-24 17:22:00 -0400

    Cleaned up optimizers.

--- autopaint/optimizers.py
+++ autopaint/optimizers.py
@@ -1,130 +1,54 @@
 import autograd.numpy as np
-import time
 
-def sga(value_and_grad,params,num_iters, alpha = .001,callback = None):
-    # Simple stochastic gradient ascent
-    D = len(params)
+def sga(gradfun, params,num_iters, alpha = .001,callback = None):
+    # Stochastic gradient ascent
     for i in xrange(num_iters):
-        val, grad = value_and_grad(params)
+        grad = gradfun(params, i)
         if callback:
-            callback(val,params,grad)
+            callback(params, i, grad)
         params = params + alpha * grad
-    val, grad = value_and_grad(params)
-    return params,val
+    return params
 
-def sga_mini_batch(batch_value_and_grad,params,batch_idxs,num_epochs, alpha = .001,callback = None):
-    # Simple stochastic gradient ascent
-    D = len(params)
-    for i in xrange(num_epochs):
-        for idxs in batch_idxs:
-            start = time.time()
-            val, grad = batch_value_and_grad(params,idxs)
-            if callback:
-                callback(val,params,grad)
-            params = params + alpha * grad
-            end = time.time()
-            print 'time per iter:', end - start
-    val, grad = batch_value_and_grad(params,idxs)
-    return params,val
-
-def adam(value_and_grad,params,num_iters, alpha = .001, beta1 = .9,beta2 = .999, eps = 1e-8,callback = None):
-    # Maximizes a stochastic function using ADAM
+def adam(gradfun, params, num_iters, alpha=0.001, beta1=0.9, beta2=0.999, eps=1e-8, callback=None):
+    # Maximizes a stochastic function using Adam.
     # http://arxiv.org/pdf/1412.6980v8.pdf
     D = len(params)
     m = np.zeros(D)
     v = np.zeros(D)
     for i in xrange(num_iters):
-        start = time.time()
-        val, grad = value_and_grad(params)
+        grad = gradfun(params, i)
         if callback:
-            callback(val,params,grad)
+            callback(params, i, grad)
         m = beta1*m + (1-beta1)*grad
         v = beta2*v + (1-beta2)*grad**2
         mhat = m/(1-beta1**(i+1))
         vhat = v/(1-beta2**(i+1))
         params = params + alpha * mhat/(np.sqrt(vhat)+eps)
-        end = time.time()
-        print 'time per iter:', end - start
-    val, grad = value_and_grad(params)
-    return params,val
+    return params
 
-
-def adam_mini_batch(batch_value_and_grad,params,batch_idxs,num_epochs, alpha = .001, beta1 = .9,beta2 = .999, eps = 1e-8,callback = None):
-    # Maximizes a stochastic function using ADAM in minibatch
-    # http://arxiv.org/pdf/1412.6980v8.pdf
-    # batch_value_and_grad is a function that returns the value and gradient of the function at params at idxs in batch_idx
-    D = len(params)
-    m = np.zeros(D)
-    v = np.zeros(D)
-    for i in xrange(num_epochs):
-        for idxs in batch_idxs:
-            start = time.time()
-            val, grad = batch_value_and_grad(params,idxs)
-            if callback:
-                callback(val,params,grad)
-            m = beta1*m + (1-beta1)*grad
-            v = beta2*v + (1-beta2)*grad**2
-            mhat = m/(1-beta1**(i+1))
-            vhat = v/(1-beta2**(i+1))
-            params = params + alpha * mhat/(np.sqrt(vhat)+eps)
-            end = time.time()
-            print 'time per iter:', end - start
-    val, grad = batch_value_and_grad(params,idxs)
-    return params,val
-
-def ada_grad_mini_batch(batch_value_and_grad,params,batch_idxs,num_epochs, alpha = 1, eps = 1e-8,callback = None):
-    # Maximizes a stochastic function using adagrad in minibatch
-    # batch_value_and_grad is a function that returns the value and gradient of the function at params at idxs in batch_idx
+def adagrad(gradfun, params, num_iters, alpha=1.0, eps=1e-8, callback=None):
+    # Maximizes a stochastic function using adagrad.
     D = len(params)
     grad_sq_hist = np.zeros(D)
-    for i in xrange(num_epochs):
-        for idxs in batch_idxs:
-            start = time.time()
-            val, grad = batch_value_and_grad(params,idxs)
-            if callback:
-                callback(val,params,grad)
-            grad_sq_hist = grad_sq_hist + grad**2
-            params = params + alpha*1/(np.sqrt(grad_sq_hist)+eps)*grad
-            end = time.time()
-            print 'time per iter:', end - start
-    val, grad = batch_value_and_grad(params,idxs)
-    return params,val
+    for i in xrange(num_iters):
+        val, grad = gradfun(params, i)
+        if callback:
+            callback(params, i, grad)
+        grad_sq_hist = grad_sq_hist + grad**2
+        params = params + alpha * 1.0/(np.sqrt(grad_sq_hist) + eps) * grad
+    return params
 
-
-def ada_delta(value_and_grad,params,num_iters, rho = .001, eps = 1e-8,callback = None):
-    # Maximizes a stochastic function using ADADELTA
+def adadelta(gradfun, params, num_iters, rho=0.001, eps=1e-8, callback=None):
     # http://arxiv.org/pdf/1212.5701v1.pdf
     D = len(params)
     g_sq = np.zeros(D)
     d_sq = np.zeros(D)
     for i in xrange(num_iters):
-        val, grad = value_and_grad(params)
+        grad = gradfun(params, i)
         if callback:
-            callback(val,params,grad)
-        g_sq = rho*g_sq + (1-rho)*grad**2
-        d = np.sqrt(d_sq+eps)/np.sqrt(g_sq+eps)*grad
-        d_sq = rho*d_sq + (1-rho)*d**2
-        params = params+d
-    val, grad = value_and_grad(params)
-    return params, val
-
-def ada_delta_mini_batch(batch_value_and_grad,params,batch_idxs,num_epochs, rho = .001, eps = 1e-8,callback = None):
-    # Maximizes a stochastic function using adagrad in minibatch
-    # batch_value_and_grad is a function that returns the value and gradient of the function at params at idxs in batch_idx
-    D = len(params)
-    g_sq = np.zeros(D)
-    d_sq = np.zeros(D)
-    for i in xrange(num_epochs):
-        for idxs in batch_idxs:
-            start = time.time()
-            val, grad = batch_value_and_grad(params,idxs)
-            if callback:
-                callback(val,params,grad)
-            g_sq = rho*g_sq + (1-rho)*grad**2
-            d = np.sqrt(d_sq+eps)/np.sqrt(g_sq+eps)*grad
-            d_sq = rho*d_sq + (1-rho)*d**2
-            params = params+d
-            end = time.time()
-            print 'time per iter:', end - start
-    val, grad = batch_value_and_grad(params,idxs)
-    return params,val
+            callback(params, i, grad)
+        g_sq = rho*g_sq + (1.0 - rho)*grad**2.0
+        d = np.sqrt(d_sq + eps)/np.sqrt(g_sq + eps)*grad
+        d_sq = rho*d_sq + (1.0 - rho)*d**2.0
+        params = params + d
+    return params
--- experiments/7-comparison/1-mnist-dglv/main.py
+++ experiments/7-comparison/1-mnist-dglv/main.py
@@ -4,30 +4,30 @@ import pickle
 import matplotlib.pyplot as plt
 
 import autograd.numpy as np
-from autograd import value_and_grad
+from autograd import grad
 
 from autopaint.flows import build_flow_sampler
 from autopaint.neuralnet import make_batches
 from autopaint.aevb import build_encoder, build_binarized_decoder
-from autopaint.optimizers import adam_mini_batch
+from autopaint.optimizers import adam
 from autopaint.util import load_and_pickle_binary_mnist, WeightsParser
 from autopaint.plotting import plot_images
 
-#AEVB params
+# Model parameters.
 param_scale = 0.1
 latent_dimension = 2
 hidden_units = 500
+num_flow_steps = 32
 
 def init_flow_params(flow_parser, rs):
-    init_output_weights = 0.1*rs.randn(num_steps, latent_dimension)
-    init_transform_weights = 0.1*rs.randn(num_steps, latent_dimension)
-    init_biases = 0.1*rs.randn(num_steps)
+    init_output_weights =    0.1*rs.randn(num_flow_steps, latent_dimension)
+    init_transform_weights = 0.1*rs.randn(num_flow_steps, latent_dimension)
+    init_biases =            0.1*rs.randn(num_flow_steps)
 
     params = np.zeros(len(flow_parser))
     flow_parser.put(params, 'output weights', init_output_weights)
     flow_parser.put(params, 'transform weights', init_transform_weights)
     flow_parser.put(params, 'biases', init_biases)
-
     return params
 
 
@@ -46,17 +46,16 @@ if __name__ == '__main__':
     N_weights_enc, encoder = build_encoder(enc_layer_sizes)
     N_weights_dec, decoder, decoder_log_like = build_binarized_decoder(dec_layer_sizes)
 
-    # Optimize aevb
+    # Optimization parameters.
     batch_size = 100
-    num_epochs = 1
-    num_steps = 32
-    num_sampler_optimization_steps = 400
+    num_training_iters = 100
     sampler_learn_rate = 0.01
+    batch_idxs = make_batches(train_images.shape[0], batch_size)
 
     init_enc_w = rs.randn(N_weights_enc) * param_scale
     init_dec_w = rs.randn(N_weights_dec) * param_scale
 
-    flow_sampler, flow_parser = build_flow_sampler(latent_dimension, num_steps)
+    flow_sampler, flow_parser = build_flow_sampler(latent_dimension, num_flow_steps)
 
     combined_parser = WeightsParser()
     combined_parser.add_shape('encoder weights', N_weights_enc)
@@ -67,39 +66,34 @@ if __name__ == '__main__':
     combined_parser.put(combined_params, 'encoder weights', init_enc_w)
     combined_parser.put(combined_params, 'flow params', init_flow_params(flow_parser, rs))
     combined_parser.put(combined_params, 'decoder weights', init_dec_w)
-    batch_idxs = make_batches(train_images.shape[0], batch_size)
 
-    def get_batch_lower_bound(cur_sampler_params, idxs):
-        #Create an initial encoding of sample images:
-        encoder_weights = combined_parser.get(cur_sampler_params, 'encoder weights')
-        flow_params     = combined_parser.get(cur_sampler_params, 'flow params')
-        decoder_weights = combined_parser.get(cur_sampler_params, 'decoder weights')
+    def get_batch_lower_bound(cur_params, iter):
+        encoder_weights = combined_parser.get(cur_params, 'encoder weights')
+        flow_params     = combined_parser.get(cur_params, 'flow params')
+        decoder_weights = combined_parser.get(cur_params, 'decoder weights')
 
-        mus, log_sigs = encoder(encoder_weights, train_images[idxs])
+        cur_data = train_images[batch_idxs[iter]]
+        mus, log_sigs = encoder(encoder_weights, cur_data)
         samples, entropy_estimates = flow_sampler(flow_params, mus, np.exp(log_sigs), rs)
-        loglike = decoder_log_like(decoder_weights, samples, train_images[idxs])
+        loglike = decoder_log_like(decoder_weights, samples, cur_data)
 
-        print "Mean loglik:", loglike.value, "Mean entropy:", np.mean(entropy_estimates.value)
+        print "Iter", iter, "loglik:", loglike.value, "entropy:", np.mean(entropy_estimates.value)
         return np.mean(entropy_estimates) + loglike
 
-    lb_val_grad = value_and_grad(get_batch_lower_bound)
+    lb_grad = grad(get_batch_lower_bound)
 
-    def callback(ml, weights, grad):
-        print "log marginal likelihood:", ml
-
+    def callback(weights, iter, grad):
         #Generate samples
         num_samples = 100
-        images_per_row = 10
         zs = rs.randn(num_samples, latent_dimension)
         samples = decoder(combined_parser.get(weights, 'decoder weights'), zs)
         fig = plt.figure(1)
         fig.clf()
         ax = fig.add_subplot(111)
-        plot_images(samples, ax, ims_per_row=images_per_row)
+        plot_images(samples, ax, ims_per_row=10)
         plt.savefig('samples.png')
 
-    final_params, final_value = adam_mini_batch(lb_val_grad, combined_params,
-                                                batch_idxs, num_epochs, callback=callback)
+    final_params, final_value = adam(lb_grad, combined_params, num_training_iters, callback=callback)
 
     finish_time = time.time()
     print "Total training time:", finish_time - start_time
