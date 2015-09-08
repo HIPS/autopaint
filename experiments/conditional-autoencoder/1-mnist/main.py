8fec27ed8686dac8fa724426daa2fd5d4702da40
naltieri <nl.altieri@gmail.com>
2015-09-08 16:46:36 -0700

    conditional autoencoder type up and running

--- experiments/conditional-autoencoder/1-mnist/main.py
+++ experiments/conditional-autoencoder/1-mnist/main.py
@@ -12,33 +12,31 @@ from scipy.optimize import minimize
 
 from autopaint.plotting import plot_images
 from autopaint.optimizers import adam
-from autopaint.neuralnet import make_batches,make_nn_funs
+from autopaint.neuralnet import make_batches,make_classification_nn
 from autopaint.util import load_mnist
 from autopaint.aevb import lower_bound
 from autopaint.util import WeightsParser, load_and_pickle_binary_mnist
 from autopaint.neuralnet import make_binary_nn,make_gaussian_nn
 param_scale = 0.1
 samples_per_image = 1
-latent_dimensions = 2
+latent_dimensions = 10
 hidden_units = 500
+
+
+def create_prob_of_data(parameters,encoder,decoder_log_like):
+    params,N_weights_enc,samples_per_image,latent_dimensions,rs = parameters
+    def data_L(data):
+        data = np.atleast_2d(data)
+        return lower_bound(params,encoder,decoder_log_like,N_weights_enc,data,samples_per_image,latent_dimensions,rs)
+    return data_L
 
 def run_aevb(train_images):
     start_time = time.time()
 
-    # Create aevb function
-    # Training parameters
-
-    D = train_images.shape[1]
-
-    enc_layers = [D, hidden_units, 2*latent_dimensions]
-    dec_layers = [latent_dimensions, hidden_units, D]
-
-    N_weights_enc, encoder, encoder_log_like = make_gaussian_nn(enc_layers)
-    N_weights_dec, decoder, decoder_log_like = make_binary_nn(dec_layers)
 
     # Optimize aevb
     batch_size = 100
-    num_training_iters = 16
+    num_training_iters = 320
     rs = npr.RandomState(0)
 
     parser = WeightsParser()
@@ -71,11 +69,11 @@ def run_aevb(train_images):
 
     final_params = adam(lb_grad, initial_combined_weights, num_training_iters, callback=callback)
 
-    def lb_fun(data):
-        lower_bound(final_params,encoder,decoder_log_like,N_weights_enc,data,samples_per_image,latent_dimensions,rs)
 
-    with open('lb_fun.pkl', 'w') as f:
-        pickle.dump(lb_fun, f, 1)
+
+    parameters = final_params,N_weights_enc,samples_per_image,latent_dimensions,rs
+    with open('parameters.pkl', 'w') as f:
+        pickle.dump(parameters, f, 1)
 
     finish_time = time.time()
     print "total runtime", finish_time - start_time
@@ -85,9 +83,20 @@ if __name__ == '__main__':
     with open('../../../autopaint/mnist_binary_data.pkl') as f:
         N_data, train_images, train_labels, test_images, test_labels = pickle.load(f)
 
+   # Create aevb function
+    # Training parameters
+
+    D = train_images.shape[1]
+
+    enc_layers = [D, hidden_units, 2*latent_dimensions]
+    dec_layers = [latent_dimensions, hidden_units, D]
+
+    N_weights_enc, encoder, encoder_log_like = make_gaussian_nn(enc_layers)
+    N_weights_dec, decoder, decoder_log_like = make_binary_nn(dec_layers)
+
     run_aevb(train_images)
-    with open('lb_fun.pkl') as f:
-        lb_fun = pickle.load(f)
+    with open('parameters.pkl') as f:
+        parameters = pickle.load(f)
 
     with open('mnist_models.pkl') as f:
         trained_weights, all_mean, all_cov = pickle.load(f)
@@ -95,25 +104,30 @@ if __name__ == '__main__':
    # Build likelihood model.
     L2_reg = 1
     layer_sizes = [784, 200, 100, 10]
-    N_weights, predict_fun, loss_fun, frac_err = make_nn_funs(layer_sizes, L2_reg)
-    classifier_loglik = lambda image, c: predict_fun(trained_weights, np.atleast_2d(image))[:, c]
+    num_weights, make_predictions, likelihood = make_classification_nn(layer_sizes)
+    classifier_loglik = lambda image, c: make_predictions(trained_weights, np.atleast_2d(image))[:, c]
 
+    data_L = create_prob_of_data(parameters,encoder,decoder_log_like)
     # Combine prior and likelihood.
-    model_ll = lambda image, c: lb_fun(image) +classifier_loglik(image, c)
-    model_ll_with_grad = value_and_grad(model_ll)
+    model_ll = lambda image, c: data_L(image) +classifier_loglik(image, c)
+
+    def model_nll(image,c):
+        return -1*model_ll(image,c)
+
+    model_nll_with_grad = value_and_grad(model_nll)
 
    # Optimize a random image to maximize this likelihood.
     cur_class = 9
     start_image = np.zeros((28*28))
-    quick_grad_check(lb_fun, start_image)
+    # quick_grad_check(data_L, start_image)
 
     def callback(image):
         #print "Cur loglik: ", image_prior_nll(image), "mean loglik:", image_prior_nll(all_mean)
-        matplotlib.image.imsave("../figures/optimizing", image.reshape((28,28)))
+        matplotlib.image.imsave("optimizing", image.reshape((28,28)))
 
     # Optimize using conjugate gradients.
-    result = minimize(-model_nll_with_grad, callback=callback, x0=start_image, args=(cur_class),
+    result = minimize(model_nll_with_grad, callback=callback, x0=start_image, args=(cur_class),
                       jac=True, method='BFGS')
     final_image = result.x
-    matplotlib.image.imsave("../figures/optimal", final_image.reshape((28,28)))
+    matplotlib.image.imsave("optimal", final_image.reshape((28,28)))
     print "Finished!"
