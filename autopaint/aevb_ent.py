f11a71013f4f2c50d69ed938cb4fb90c198528b7
naltieri <nl.altieri@gmail.com>
2015-09-12 12:03:42 -0700

    added some more debugging code to help understand auto-ent

--- autopaint/aevb_ent.py
+++ autopaint/aevb_ent.py
@@ -1,5 +1,6 @@
 import autograd.numpy as np
 from autopaint.util import entropy_of_a_diagonal_gaussian
+from plotting import plot_density
 
 def lower_bound(weights,encode,decode_log_like,log_prior,N_weights_enc,train_images,samples_per_image,latent_dimensions,rs):
     enc_w = weights[0:N_weights_enc]
@@ -11,14 +12,20 @@ def lower_bound(weights,encode,decode_log_like,log_prior,N_weights_enc,train_ima
 
 def compute_log_prob_and_ent(enc_w,dec_w,encode,decode_log_like,log_prior,train_images,samples_per_image,latent_dimensions,rs):
     (mus,log_sigs) = encode(enc_w,train_images)
-    log_sigs = np.ones(log_sigs.shape)
     sigs = np.exp(log_sigs)
+    sigs = np.ones(sigs.shape)
     ents = entropy_of_a_diagonal_gaussian(sigs)
     mean_ent = np.mean(ents)
     noise = rs.randn(samples_per_image,train_images.shape[0],latent_dimensions)
     Z_samples = mus + sigs*noise
     Z_samples = np.reshape(Z_samples,(train_images.shape[0]*samples_per_image,latent_dimensions),order = 'F')
     train_images_repeat = np.repeat(train_images,samples_per_image,axis=0)
+    print 'prior avg', np.mean(log_prior(Z_samples))
+    try:
+        plot_samples = Z_samples.value
+    except:
+        plot_samples = Z_samples
+    plot_density(plot_samples,'z_samples.png')
     mean_log_prob = np.mean(decode_log_like(dec_w,Z_samples,train_images_repeat) +log_prior(Z_samples))
     return mean_log_prob, mean_ent
 
